1c9381135c4a4aff7ec780604f68c70b13d81d56
Ryan Newton <newton1988@gmail.com>
2017-01-23 19:22:04 -0500

    Adds reusable components

---
+++ Mobile/src/components/common/Header.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{props.headerText}</Text>
+		</View>
+	);
+};
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		elevation: 3,
+		position: 'relative'
+	},
+	textStyle: {
+		fontSize: 20
+	}
+};
+
+export { Header };
