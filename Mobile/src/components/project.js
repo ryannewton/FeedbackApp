a2a567d4d9725dd6dad11e82045a8700f5c0c4b2
Ryan Newton <newton1988@gmail.com>
2017-02-04 18:34:17 -0500

    Updates styling on projects component

--- Mobile/src/components/project.js
+++ Mobile/src/components/project.js
@@ -8,7 +8,7 @@ import { View, Text, TouchableHighlight } from 'react-native';
 //import Button from '../components/button.js';
 import ProjectDetails from '../scenes/project_details.js';
 import styles from '../styles/styles_main.js';
-import { Button } from './common';
+import { Button, Card } from './common';
 
 class Project extends Component {
 	constructor(props) {
@@ -29,51 +29,59 @@ class Project extends Component {
 	}
 
 	// Temporary fix. Async issue is causing this.props.item to be temporarily undefined
-	renderDescription() {
-		if(this.props.item === undefined) {
+	renderVoteCount() {
+		if (this.props.item === undefined) {
 			return '';
 		}
-
-		return `${this.props.item.votes} Votes: `;
+		return `${this.props.item.votes} Votes`;
 	}
 
 	// Temporary fix. Async issue is causing this.props.item to be temporarily undefined
 	renderTitle() {
-		if(this.props.item === undefined) {
+		if (this.props.item === undefined) {
 			return '';
 		}
-
 		return this.props.item.title;
 	}
 
 	render() {
-			return (
+		const { buttonText, lowWeight, row } = styles;
+
+		return (
+			<Card>
 				<TouchableHighlight
-					style={styles.row}
+					style={row}
 					underlayColor='#D0D0D0'
 					onPress={this.goToDetails}
 				>
-					<View style={styles.project}>
 
-						{/* Add description of Text content */}
-						<Text style={[styles.buttonText, styles.low_weight]}>
-							{this.renderDescription()}
-						</Text>
-
-						{/* Add description of Text content */}
-						<Text style={styles.buttonText}>
+					<View style={{ justifyContent: 'flex-start' }}>
+						{/* Project title */}
+						<Text style={buttonText}>
 							{this.renderTitle()}
 						</Text>
 
-						{/* Button for upvoting a piece of feedback */}
-						<View>
-							<Button	onPress={this.upvote}>
-								Up Vote!
-							</Button>
+						{/* Vote section */}
+						<View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 5 }}>
+							{/* Vote count */}
+							<View style={{ flex: 1 }}>
+								<Text style={[buttonText, lowWeight]}>
+									{this.renderVoteCount()}
+								</Text>
+							</View>
+
+							{/* Upvote button */}
+							<View style={{ flex: 1, alignItems: 'flex-end' }}>
+								<Button	onPress={this.upvote} style={{ width: 80, height: 27, marginRight: 2 }}>
+									Upvote!
+								</Button>
+							</View>
 						</View>
 					</View>
+
 				</TouchableHighlight>
-			);
+			</Card>
+		);
 	}
 }
 
