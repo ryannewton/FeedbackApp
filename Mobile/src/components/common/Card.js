4e2da7bd9992f6c86b524f8b7994ca73a59ef093
Ryan Newton <newton1988@gmail.com>
2017-02-05 16:18:32 -0500

    Adds styling on Card component for iOS

--- Mobile/src/components/common/Card.js
+++ Mobile/src/components/common/Card.js
@@ -1,15 +1,13 @@
 'use strict';
 
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 
-const Card = (props) => {
-	return (
-		<View style={styles.containerStyle}>
-			{props.children}
-		</View>
-	);
-};
+const Card = (props) => (
+	<View style={styles.containerStyle}>
+		{props.children}
+	</View>
+);
 
 const styles = {
 	containerStyle: {
@@ -20,7 +18,15 @@ const styles = {
 		elevation: 1,
 		marginLeft: 4,
 		marginRight: 4,
-		marginTop: 4
+		marginTop: 4,
+		...Platform.select({
+			ios: {
+				shadowColor: '#000',
+				shadowOffset: { width: 0, height: 2 },
+				shadowOpacity: 0.1,
+				shadowRadius: 2,
+			}
+		})
 	}
 };
 
