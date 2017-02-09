4abf6594b2388ba7b35e92620a9c52c1bccaa890
Ryan Newton <newton1988@gmail.com>
2017-02-09 13:56:17 -0500

    Adds scaffolding to Signup scene

--- Mobile/src/scenes/signup.js
+++ Mobile/src/scenes/signup.js
@@ -4,10 +4,44 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 
+// Import components
+import { Card, CardSection, Input, Button } from '../components/common';
+
 class Signup extends Component {
 	render() {
 		return (
-			<Text>Placeholder</Text>
+			<Card>
+				{/* Email input */}
+				<CardSection>
+					<Input
+						label="Email"
+						placeholder="joe@gmail.com"
+					/>
+				</CardSection>
+
+				{/* Password input */}
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Password"
+						placeholder="password"
+					/>
+				</CardSection>
+
+				{/* Password confirm input */}
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Confirm Password"
+						placeholder="password"
+					/>
+				</CardSection>
+
+				{/* Confirmation button */}
+				<Button>
+					Signup
+				</Button>
+			</Card>
 		);
 	}
 }
