98102ba62c3281ed6fdfd592955106a6057bc72f
Ryan Newton <newton1988@gmail.com>
2017-02-09 13:42:56 -0500

    Boilerplace for Signup scene

---
+++ Mobile/src/scenes/signup.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Import Libraries
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Signup extends Component {
+	render() {
+		return (
+			<Text>Placeholder</Text>
+		);
+	}
+}
+
+export default Signup;
