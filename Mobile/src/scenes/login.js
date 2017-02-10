85182e378ba27ca3ce58ed345ea3f5aaa607dd9d
Ryan Newton <newton1988@gmail.com>
2017-02-09 19:39:36 -0500

    Boilerplate for Login scene

---
+++ Mobile/src/scenes/login.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Import libraries
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Login extends Component {
+	render() {
+		return (
+			<Text>Placeholder</Text>
+		);
+	}
+}
+
+export default Login;
