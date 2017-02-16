0391f45f04a219bc71136cb9ec4c77d830595e5a
Ryan Newton <newton1988@gmail.com>
2017-02-16 18:28:44 -0500

    Adds user to state; tracks projects user upvoted

--- Mobile/src/reducers/reducer_index.js
+++ Mobile/src/reducers/reducer_index.js
@@ -6,10 +6,12 @@ import main from './reducer_main.js';
 import navigation from './reducer_navigation.js';
 import projects from './reducer_projects.js';
 import auth from './auth_reducer';
+import user from './user_reducer';
 
 export default combineReducers({
 	main,
 	navigation,
 	projects,
-	auth
+	auth,
+	user
 });
