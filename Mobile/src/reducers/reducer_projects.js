6a314c657676e2a34731d45fdd11b548d4e40cdc
Ryan Newton <newton1988@gmail.com>
2017-02-18 16:05:44 -0500

    Fixes bug throwing error when action.payload did not contain an array of upvotes

--- Mobile/src/reducers/reducer_projects.js
+++ Mobile/src/reducers/reducer_projects.js
@@ -10,23 +10,29 @@ import {
 } from '../actions/types';
 
 export default (state = [], action) => {
-	const index = state.findIndex((project) => project.id === action.payload.id);
-	let newState = state.slice(0);
-
 	switch (action.type) {
 		case REQUESTED_PROJECTS:
 			return state;
 		case RECEIVED_PROJECTS:
 			return action.payload;
-		case SAVE_PROJECT_CHANGES:
+		case SAVE_PROJECT_CHANGES: {
+			const index = state.findIndex((project) => project.id === action.payload.id);
+			const newState = state.slice(0);
 			newState.splice(index, 1, action.payload);
 			return newState;
-		case ADD_UP_VOTE:
+		}
+		case ADD_UP_VOTE: {
+			const index = state.findIndex((project) => project.id === action.payload.id);
+			const newState = state.slice(0);
 			newState[index].votes += 1;
 			return newState;
-		case REMOVE_UP_VOTE:
+		}
+		case REMOVE_UP_VOTE: {
+			const index = state.findIndex((project) => project.id === action.payload.id);
+			const newState = state.slice(0);
 			newState[index].votes -= 1;
 			return newState;
+		}
 		default:
 			return state;
 	}
