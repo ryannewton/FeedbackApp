baf2e8eb11cede386dc81ea0f4a535cd8f3d6913
Ryan Newton <newton1988@gmail.com>
2017-02-23 18:49:27 -0500

    Adds project reducers for addUpVote and removeUpVote

--- Mobile/src/reducers/reducer_projects.js
+++ Mobile/src/reducers/reducer_projects.js
@@ -4,7 +4,9 @@
 import {
 	REQUESTED_PROJECTS,
 	RECEIVED_PROJECTS,
-	SAVE_PROJECT_CHANGES
+	SAVE_PROJECT_CHANGES,
+	ADD_UP_VOTE,
+	REMOVE_UP_VOTE
 } from '../actions/types';
 
 export default (state = [], action) => {
@@ -17,6 +19,16 @@ export default (state = [], action) => {
 			const index = state.findIndex((project) => project.id === action.payload.id);
 			const newState = state.slice(0);
 			newState.splice(index, 1, action.payload);
+			return newState;
+		case ADD_UP_VOTE:
+			const index = state.findIndex((project) => project.id === action.payload.id);
+			const newState = state.slice(0);
+			newState[index].votes += 1;
+			return newState;
+		case REMOVE_UP_VOTE:
+			const index = state.findIndex((project) => project.id === action.payload.id);
+			const newState = state.slice(0);
+			newState[index].votes -= 1;
 			return newState;
 		default:
 			return state;
