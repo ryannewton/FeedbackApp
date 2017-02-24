4601f33d5f97258cd44b2368e667928b1a0dc291
Ryan Newton <newton1988@gmail.com>
2017-02-23 19:00:22 -0500

    Adds user to state; tracks projects user upvoted

---
+++ Mobile/src/reducers/user_reducer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Import action types
+import {
+	ADD_UP_VOTE,
+	REMOVE_UP_VOTE
+} from '../actions/types';
+
+const INITIAL_STATE = {
+	upvotes: []
+};
+
+export default (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case ADD_UP_VOTE:
+			return { ...state, upvotes: [...state.upvotes, action.payload.id] };
+		case REMOVE_UP_VOTE:
+			return { ...state, upvotes: removeItem(state.upvotes, action.payload.id) };
+		default:
+			return state;
+	}
+};
+
+const removeItem = (arr, item) => {
+	const index = arr.indexOf(item);
+	if (index !== -1) {
+		return [...arr.slice(0, index), ...arr.slice(index + 1)];
+	}
+	return arr;
+}
