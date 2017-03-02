a003046997990fe619089d7614465e4048a4a217
Ryan Newton <newton1988@gmail.com>
2017-03-01 19:40:56 -0500

    Adds structure for Project Details scene

--- Mobile/src/scenes/project_details.js
+++ Mobile/src/scenes/project_details.js
@@ -2,55 +2,121 @@
 
 //Import Libraries
 import React, { Component } from 'react';
-import { View, Button, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-//Import actions
-import * as actions from '../actions';
 
 //Import componenets, functions, and styles
 import styles from '../styles/styles_main.js';
+import { addUpvote, removeUpvote } from '../actions';
+import { Button, Header, Card, CardSection } from '../components/common';
 
-class Project_Details extends Component {
+class ProjectDetails extends Component {
 	constructor(props) {
 		super(props);
-	}
 
-	on_back() {
-
+		const { project } = this.props.navigationState.routes.slice(-1)[0];
+		this.state = { project };
 	}
 
 	upvote() {
+		const { user } = this.props;
+		const { project } = this.state;
+		// If user hasn't upvoted this project, add an upvote
+		if (!user.upvotes.includes(project.id)) {
+			this.props.addUpvote(project);
+		} else {
+			this.props.removeUpvote(project);
+		}
+	}
 
+	projectDescription() {
+		const { buttonText, lowWeight } = styles;
+		return (
+			<View style={{ justifyContent: 'flex-start' }}>
+				{/* Project title */}
+				<Text style={buttonText}>
+					{this.state.project.title}
+				</Text>
+
+				{/* Vote section */}
+				<View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 5 }}>
+					{/* Vote count */}
+					<View style={{ flex: 3 }}>
+						<Text style={[buttonText, lowWeight]}>
+							{`${this.state.project.votes} Votes`}
+						</Text>
+					</View>
+
+					{/* Upvote button */}
+					<View style={{ flex: 1, alignItems: 'flex-end' }}>
+						{this.renderButton()}
+					</View>
+				</View>
+			</View>
+		);
+	}
+
+	renderButton() {
+		const { user } = this.props;
+		const { project } = this.state;
+		let buttonStyles = { width: 80, height: 27, marginRight: 2 };
+		let textStyles = { paddingTop: 10, paddingBottom: 10 };
+		// If user hasn't upvoted this project
+		if (user.upvotes.includes(project.id)) {
+			buttonStyles = { ...buttonStyles, backgroundColor: '#007aff' };
+			textStyles = { ...textStyles, color: '#fff' };
+		}
+		return (
+			<Button
+				onPress={this.upvote.bind(this)}
+				style={buttonStyles}
+				textStyle={textStyles}
+			>
+				Upvote!
+			</Button>
+		);
 	}
 
 	render() {
-	const { project, buttonText, lowWeight } = styles;
+		const { container } = styles;
 		return (
-			<View style={project}>
-				<Text style={[buttonText, lowWeight]}>
-					{this.props.scene.route.item.votes} Votes: 
-				</Text>
-				<Text style={buttonText}>
-					{this.props.scene.route.item.title}
-				</Text>
-				<View>
-					<Button	onPress={this.upvote}>
-						Upvote!
-					</Button>
+			<TouchableWithoutFeedback style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
+				<View style={container}>
+
+					<Header>
+						Projects Details
+					</Header>
+
+					<Card>
+						<CardSection>
+							{this.projectDescription()}
+						</CardSection>
+					</Card>
+
+					<Card>
+						<CardSection>
+							<Text>Suggested solutions:</Text>
+						</CardSection>
+					</Card>
+
+					<Card>
+						<CardSection>
+							<Text style={{ textAlign: 'center' }}>Add a solution:</Text>
+						</CardSection>
+						<CardSection>
+							<TextInput />
+						</CardSection>
+
+					</Card>
 				</View>
-			</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
 
 function mapStateToProps(state) {
-	return state;
+	const { user } = state;
+	return { user };
 }
 
-function mapDispatchToProps(dispatch) {
-	return bindActionCreators(actions, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Project_Details);
+export default connect(mapStateToProps, { addUpvote, removeUpvote })(ProjectDetails);
