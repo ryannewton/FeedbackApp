0e11c92625dd20d7932dbcc5b021ecfaa4685f17
Ryan Newton <newton1988@gmail.com>
2017-03-04 18:20:13 -0500

    Starts a folder for component-level styling

---
+++ Mobile/src/styles/common/input_styles.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export const inputText = {
+	fontSize: 18,
+	borderColor: 'gray',
+	borderWidth: 1,
+	borderRadius: 4,
+	margin: 3,
+	marginVertical: 4,
+	marginHorizontal: 4,
+	paddingVertical: 8,
+	paddingHorizontal: 8,
+	backgroundColor: '#FFFFFF'
+};
