e015b37345ef9d1c1e064e0ea5d5b83475686390
Ryan Newton <newton1988@gmail.com>
2017-03-04 19:15:29 -0500

    Reducer boilerplate for solutions reducer

---
+++ Mobile/src/reducers/solutions_reducer.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Import action types
+import {
+} from '../actions/types';
+
+export default (state = [], action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
