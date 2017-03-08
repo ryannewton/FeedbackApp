7bc571d01365677c151c8cf779ff9c846f04434a
Tyler Hannasch <Tyler Hannasch>
2017-03-08 12:50:33 -0800

    adding backend access for apple, updating icons

--- Mobile/src/constants.js
+++ Mobile/src/constants.js
@@ -2,8 +2,8 @@
 
 export const ROOT_STORAGE = '@FeedbackApp:';
 export const ROOT_URL = 'https://feedbackappdev.com';
-// export const ROOT_URL = 'https://collaborativefeedback.com';
-// export const ROOT_URL = 'http://10.0.2.2:3000';
+//export const ROOT_URL = 'https://collaborativefeedback.com';
+//export const ROOT_URL = 'http://10.0.2.2:3000';
 //export const ROOT_URL = 'http://localhost:3000';
 
 
