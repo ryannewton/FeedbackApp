169f1a1f48bb38fcc2a63f8be025fd243314704a
Tyler Hannasch <Tyler Hannasch>
2017-03-22 13:55:21 -0700

    clean up linter

--- Mobile/src/reducers/reducer_main.js
+++ Mobile/src/reducers/reducer_main.js
@@ -1,31 +1,31 @@
 // Import action types
 import {
-	FEEDBACK_CHANGED,
-	SUBMIT_FEEDBACK,
-	SUBMIT_FEEDBACK_SUCCESS,
-	SUBMIT_FEEDBACK_FAIL,
-	SOLUTION_CHANGED,
+  FEEDBACK_CHANGED,
+  SUBMIT_FEEDBACK,
+  SUBMIT_FEEDBACK_SUCCESS,
+  SUBMIT_FEEDBACK_FAIL,
+  SOLUTION_CHANGED,
 } from '../actions/types';
 
 const INITIAL_STATE = {
-	loading: false,
-	feedback: '',
+  loading: false,
+  feedback: '',
 };
 
 export default (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case FEEDBACK_CHANGED:
-			return { ...state, feedback: action.payload };
-		case SUBMIT_FEEDBACK:
-			return { ...state, loading: true };
-		case SUBMIT_FEEDBACK_SUCCESS:
-			return { ...state, loading: false, feedback: '' };
-		case SUBMIT_FEEDBACK_FAIL:
-			return { ...state, loading: false };
-		case SOLUTION_CHANGED:
-			return { ...state, solution: action.payload };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case FEEDBACK_CHANGED:
+      return { ...state, feedback: action.payload };
+    case SUBMIT_FEEDBACK:
+      return { ...state, loading: true };
+    case SUBMIT_FEEDBACK_SUCCESS:
+      return { ...state, loading: false, feedback: '' };
+    case SUBMIT_FEEDBACK_FAIL:
+      return { ...state, loading: false };
+    case SOLUTION_CHANGED:
+      return { ...state, solution: action.payload };
+    default:
+      return state;
+  }
 };
 
