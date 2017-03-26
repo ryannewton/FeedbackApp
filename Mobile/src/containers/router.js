42f4e8305ccca460c707a60a2da02ff205ba57ef
Ryan Newton <newton1988@gmail.com>
2017-03-26 02:31:08 -0400

    Links button in Feedback header to Settings scene

--- Mobile/src/containers/router.js
+++ Mobile/src/containers/router.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
-import { Icon } from 'react-native-elements';
+// import { Icon } from 'react-native-elements';
 
 // Import Scenes
 import Feedback from '../scenes/Feedback';
@@ -13,6 +13,56 @@ import Settings from '../scenes/Settings';
 
 // Import Components
 import { Button } from '../components/common';
+
+// Import icons
+import FeedbackSelected from '../../images/icons/feedback2-selected_100px.png';
+import FeedbackNotSelected from '../../images/icons/feedback2-notselected_100px.png';
+import NewProjectsSelected from '../../images/icons/newprojects2-selected_100px.png';
+import NewProjectsNotSelected from '../../images/icons/newprojects2-notselected_100px.png';
+import AllProjectsSelected from '../../images/icons/allprojects4-selected_100px.png';
+import AllProjectsNotSelected from '../../images/icons/allprojects4-notselected_100px.png';
+
+function navButton(target, label, navigate) {
+  const right = (
+    <View style={{ width: 80 }}>
+      <Button
+        onPress={() => {
+          navigate(target);
+        }}
+      >
+        {label}
+      </Button>
+    </View>
+  );
+
+  return { right };
+}
+
+export const FeedbackStack = StackNavigator({
+  Feedback: {
+    screen: Feedback,
+    navigationOptions: {
+      title: 'Send Feedback',
+      header: ({ navigate }) => navButton('Settings', 'Settings', navigate),
+      icon: FeedbackSelected,
+    },
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      title: 'Settings',
+    },
+  },
+});
+
+export const NewProjectsStack = StackNavigator({
+  NewProjects: {
+    screen: NewProjects,
+    navigationOptions: {
+      title: 'New Projects',
+    },
+  },
+});
 
 export const ProjectsStack = StackNavigator({
   Projects: {
@@ -25,46 +75,6 @@ export const ProjectsStack = StackNavigator({
     screen: ProjectDetails,
     navigationOptions: {
       title: 'Project Details',
-    },
-  },
-});
-
-function settingsButton({ state, setParams }) {
-  let right = (
-    <View style={{ width: 80 }}>
-      <Button onPress={() => setParams({ mode: 'settings' })} >
-        Settings
-      </Button>
-    </View>
-  );
-  if (state.params.mode === 'settings') {
-    right = (
-      <View style={{ width: 80 }}>
-        <Button onPress={() => setParams({ mode: 'none' })} >
-          Done
-        </Button>
-      </View>
-    );
-  }
-
-  return { right };
-}
-
-export const FeedbackStack = StackNavigator({
-  Feedback: {
-    screen: Feedback,
-    navigationOptions: {
-      title: 'Send Feedback',
-      header: settingsButton,
-    },
-  },
-});
-
-export const NewProjectsStack = StackNavigator({
-  NewProjects: {
-    screen: NewProjects,
-    navigationOptions: {
-      title: 'New Projects',
     },
   },
 });
