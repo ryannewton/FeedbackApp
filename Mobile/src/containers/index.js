9286cb2299bcb40ced1a746f1d302ab3715ebb49
Ryan Newton <ryannewton@Ryans-MacBook-Pro.local>
2017-06-08 10:55:25 -0700

    Stops react-navigation console.logs

--- Mobile/src/containers/index.js
+++ Mobile/src/containers/index.js
@@ -18,7 +18,7 @@ class Index extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router />
+        <Router onNavigationStateChange={null} />
       </Provider>
     );
   }
