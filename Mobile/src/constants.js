3cfa476a69261b425f2d47da901f2f65064ed72e
Ryan Newton <ryannewton@Ryans-MacBook-Pro.local>
2017-06-28 15:10:17 -0700

    Adds new Admin Console to Server/public/

--- Mobile/src/constants.js
+++ Mobile/src/constants.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
 
 export const ROOT_STORAGE = '@FeedbackApp:';
-export const ROOT_URL = 'https://feedbackappdev.com';
-// export const ROOT_URL = 'https://collaborativefeedback.com';
+// export const ROOT_URL = 'https://feedbackappdev.com';
+export const ROOT_URL = 'https://collaborativefeedback.com';
 // export const ROOT_URL = 'http://10.0.2.2:3000';
 // export const ROOT_URL = 'http://localhost:3000';
 
