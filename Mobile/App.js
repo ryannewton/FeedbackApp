a45b2c8a344d3c44f4c388cba9bd02daf73ed2d9
Tyler Hannasch <tyler.hannasch@gmail.com>
2017-07-02 22:35:40 -0700

    fixes merge conflicts and errors in two pulls

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Index from './src/containers/index';
+import { Font } from 'expo';
+import { Text } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Arial': require('./assets/fonts/arial.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
     return (
-      <Index />
+      this.state.fontLoaded ? (
+        <Index />
+      ) : null
     );
   }
 }
