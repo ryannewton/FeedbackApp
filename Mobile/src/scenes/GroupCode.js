49e3bd25dd991c84fba16b277f71e07de63cf3c0
Alice Zheng <alicezheng@Alices-MacBook-Pro-3.local>
2017-07-12 09:54:24 -0700

    add bg for auth3

--- Mobile/src/scenes/GroupCode.js
+++ Mobile/src/scenes/GroupCode.js
@@ -14,7 +14,7 @@ import styles from '../styles/scenes/AuthorizeStyles';
 
 import FontAwesomeIcon from '@expo/vector-icons/FontAwesome';
 import { Makiko } from 'react-native-textinput-effects';
-import fullScreen from '../../images/backgrounds/auth2.jpg';
+import fullScreen from '../../images/backgrounds/auth3.jpg';
 
 class Authorize extends Component {
   constructor(props) {
