a913a701d7e22181698a901d66659d91ca2a4c3a
Tyler Hannasch <tyler.hannasch@gmail.com>
2017-07-24 11:39:00 -0700

    Deletes admin2

--- Admin2/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export * from './actions_auth';
-export * from './actions_feedback';
-export * from './actions_main';
-export * from './actions_projectAdditions';
-export * from './actions_projects';
-export * from './actions_user';
