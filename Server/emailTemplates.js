9efe1fbbc72fc5c6480452618bd9c3aefed1d51d
Alice Zheng <alicezheng@Alices-MacBook-Pro-3.local>
2017-07-25 14:05:04 -0700

    new email text

--- Server/emailTemplates.js
+++ Server/emailTemplates.js
@@ -13,56 +13,27 @@ Note: Your contact information has been kept confidential. This message was writ
 
 export function rejectedFeedback({ feedback, message }) {
   const subjectLine = 'Suggestion Box: Feedback rejected';
-  const bodyText = `In response to your feedback:\n
-${feedback.text}\n
-\n
-Your feedback was rejected because:\n
-${message}\n
-\n
-Note: Your contact information has been kept confidential. This message was written without knowledge of who sent the feedback.`;
-
+  const bodyText =
+    `Hi! This is your friend at Suggestion Box.\n\nThank you for submitting a feedback with Suggestion Box! Sadly, your feedback: "${feedback.text}" was rejected by your administrator.\n\nThe explanation we received was: "${message}"\n\nThere is no need to worry. Your contact information has been kept confidential. This message was written without knowledge of who sent the feedback.\n\nPlease don't let this rejection stop you from sending the next feedback! I hope to here your thoughts again soon!\n\nYour friend at Suggestion Box.`;
   return { subjectLine, bodyText };
 }
 
 export function rejectedSolution({ solution, message }) {
   const subjectLine = 'Suggestion Box: Solution rejected';
-  const bodyText = `In response to your Solution:\n
-${solution.text}\n
-\n
-Your solution was rejected because:\n
-${message}\n
-\n
-Note: Your contact information has been kept confidential. This message was written without knowledge of who sent the feedback.`;
-
+  const bodyText = `Hi! This is your friend at Suggestion Box.\n\nThank you for proposing a solution with Suggestion Box! Sadly, your submission: "${solution.text}" was rejected by your administrator.\n\nThe explanation we received was: "${message}"\n\nThere is no need to worry. Your contact information has been kept confidential. This message was written without knowledge of who sent the solution.\n\nPlease don't let this rejection stop you from sending the next solution or feedback! I hope to here your thoughts again soon!\n\nYour friend at Suggestion Box.`;
   return { subjectLine, bodyText };
 }
 
 export function clarifyFeedback({ feedback, message }) {
   const subjectLine = 'Suggestion Box: Clarification needed';
-  const bodyText = `In response to your feedback:\n
-${feedback.text}\n
-\n
-Some clarification is needed:\n
-${message}\n
-\n
-You can reply to this email and your message will be passed along.\n
-\n
-Note: Your contact information has been kept confidential. This message was written without knowledge of who sent the feedback.`;
-
+  const bodyText =
+    `Hi! This is your friend at Suggestion Box.\n\nThank you for submitting a feedback with Suggestion Box! Your administrator has requested for a clarification on your feedback: "${feedback.text}".\n\nThe note we received from your administrator was: "${message}"\n\nYour contact information has been kept confidential. This message was written without knowledge of who sent the feedback.\n\nYou can reply to this email and your message will be passed along. I hope to here more of your thoughts in the future!\n\nYour friend at Suggestion Box.`;
   return { subjectLine, bodyText };
 }
 
 export function clarifySolution({ solution, message }) {
   const subjectLine = 'Suggestion Box: Clarification needed';
-  const bodyText = `In response to your solution:\n
-${solution.text}\n
-\n
-Some clarification is needed:\n
-${message}\n
-\n
-You can reply to this email and your message will be passed along.\n
-\n
-Note: Your contact information has been kept confidential. This message was written without knowledge of who sent the feedback.`;
-
+  const bodyText =
+      `Hi! This is your friend at Suggestion Box.\n\nThank you for proposing a solution with Suggestion Box! Your administrator has requested for a clarification on your solution: "${solution.text}".\n\nThe note we received from your administrator was: "${message}"\n\nYour contact information has been kept confidential. This message was written without knowledge of who sent the solution.\n\nYou can reply to this email and your message will be passed along. I hope to here more of your thoughts in the future!\n\nYour friend at Suggestion Box.`;
   return { subjectLine, bodyText };
 }
