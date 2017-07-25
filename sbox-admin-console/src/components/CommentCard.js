78e6d1a4c3ca99466a5d95d8a6be9aa5db99504c
Tyler Hannasch <tyler.hannasch@gmail.com>
2017-07-25 16:05:54 -0700

    small changes

---
+++ sbox-admin-console/src/components/CommentCard.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Panel, Glyphicon } from 'react-bootstrap';
+import TimeAgo from 'react-timeago'
+
+class CommentCard extends Component {
+
+  renderVotesAndTime = () => {
+    return (
+      <div className="row" style={{marginLeft:6, marginRight:6}}>
+        <div className="pull-left">
+          <Glyphicon glyph='triangle-top' /><span style={{margin:5}}>{this.props.solution.upvotes}</span><Glyphicon glyph='triangle-bottom' /><span style={{margin:5}}>{this.props.solution.downvotes}</span>
+        </div>
+        <div className="pull-right">
+          <TimeAgo date={this.props.solution.date} />
+        </div>
+      </div>
+    );
+  }
+
+  renderText = () => {
+    return (
+      <div className="row" style={{marginLeft:6, marginRight:6}}>{this.props.solution.text}</div>
+    );
+  }
+
+  render() {
+    console.log(this.props.solution);
+    return (
+      <Panel>
+        {this.renderVotesAndTime()}
+        {this.renderText()}
+      </Panel>
+    );
+  }
+}
+
+export default CommentCard;
