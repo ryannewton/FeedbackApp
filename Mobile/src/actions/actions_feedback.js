723f9f318951d836f3800170a815c87f1b9677c4
James Baker <jbaker1@mit.edu>
2017-07-28 14:40:35 -0700

    Update actions_feedback.js

--- Mobile/src/actions/actions_feedback.js
+++ Mobile/src/actions/actions_feedback.js
@@ -58,12 +58,12 @@ export const submitFeedbackToServer = (feedbackRequiresApproval, text, type, ima
 
       // Automatically upvote feedback the user submitted
       feedback = { ...feedback, id: response.data.id, status: 'new', trendingScore: 1, upvotes: 0, downvotes: 0, noOpinions: 0, approved: 1, date: Date.now()};
-      dispatch(addFeedbackUpvote(feedback));
-
+      
       // Add to local state if no approval required
       if (!feedbackRequiresApproval) {
         dispatch({ type: ADD_FEEDBACK_TO_STATE, payload: feedback });
       }
+      dispatch(addFeedbackUpvote(feedback));
     })
     .catch((error) => {
       console.log('Error in submitFeedbackToServer in actions_feedback', error);
