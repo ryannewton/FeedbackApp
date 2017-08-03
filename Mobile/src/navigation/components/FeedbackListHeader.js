c6aff469ff3fb05c783e95810dfbbc2b15372c9b
Alice Zheng <alicezheng@Alices-MacBook-Pro-3.local>
2017-08-02 18:12:42 -0700

    don't render filter when there is no category

--- Mobile/src/navigation/components/FeedbackListHeader.js
+++ Mobile/src/navigation/components/FeedbackListHeader.js
@@ -129,7 +129,9 @@ class FeedbackSubmitHeader extends Component {
 
   renderPicker = () => {
     // Exclude on Android, library doesn't work
-
+    if (!this.props.group.category) {
+      return null;
+    }
     const renderTouchable = () => <TouchableOpacity />;
     const { menuOptions, divider, pickerStyle } = styles;
     return (
