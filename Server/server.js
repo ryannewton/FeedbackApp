408d9f8c2b8bad9aa8f65b326e0fe58ffb19ad2e
Tyler Hannasch <tyler.hannasch@gmail.com>
2017-08-03 17:49:23 -0700

    new emails #1

--- Server/server.js
+++ Server/server.js
@@ -1215,6 +1215,20 @@ Note: Your contact information has been kept confidential. This message was writ
   return { subjectLine, bodyText };
 }
 
+function clarifyFeedback({ feedback, message, adminEmail }) {
+  const subjectLine = `Suggestion Box: Clarification needed on Suggestion, "${feedback.text.slice(0, 10)}"`;
+  const bodyText =
+    `Hi! This is your friend at Suggestion Box.\n\nThank you for submitting a feedback with Suggestion Box! Your administrator has requested for a clarification on your feedback: "${feedback.text}".\n\nThe note we received from your administrator was: "${message}"\n\nYour contact information has been kept confidential. This message was written without knowledge of who sent the feedback.\n\nHere is your administrator's email if you want to reply: ${adminEmail}. I hope to here more of your thoughts in the future!\n\nYour friend at Suggestion Box.`;
+  return { subjectLine, bodyText };
+}
+
+function clarifySolution({ solution, message, adminEmail }) {
+  const subjectLine = 'Suggestion Box: Clarification needed';
+  const bodyText =
+      `Hi! This is your friend at Suggestion Box.\n\nThank you for proposing a solution with Suggestion Box! Your administrator has requested for a clarification on your solution: "${solution.text}".\n\nThe note we received from your administrator was: "${message}"\n\nYour contact information has been kept confidential. This message was written without knowledge of who sent the solution.\n\nHere is your administrator's email if you want to reply: ${adminEmail}. I hope to here more of your thoughts in the future!\n\nYour friend at Suggestion Box.`;
+  return { subjectLine, bodyText };
+}
+
 function rejectFeedback({ feedback, message, adminEmail }) {
   const subjectLine = 'Suggestion Box: Feedback rejected';
   const bodyText =
@@ -1225,20 +1239,6 @@ function rejectFeedback({ feedback, message, adminEmail }) {
 function rejectSolution({ solution, message, adminEmail }) {
   const subjectLine = 'Suggestion Box: Solution rejected';
   const bodyText = `Hi! This is your friend at Suggestion Box.\n\nThank you for proposing a solution with Suggestion Box! Sadly, your submission: "${solution.text}" was rejected by your administrator.\n\nThe explanation we received was: "${message}"\n\nThere is no need to worry. Your contact information has been kept confidential. This message was written without knowledge of who sent the solution. Here is your administrator's email if you want to reply: ${adminEmail}.\n\nPlease don't let this rejection stop you from sending the next solution or feedback! I hope to here your thoughts again soon!\n\nYour friend at Suggestion Box.`;
-  return { subjectLine, bodyText };
-}
-
-function clarifyFeedback({ feedback, message, adminEmail }) {
-  const subjectLine = 'Suggestion Box: Clarification needed';
-  const bodyText =
-    `Hi! This is your friend at Suggestion Box.\n\nThank you for submitting a feedback with Suggestion Box! Your administrator has requested for a clarification on your feedback: "${feedback.text}".\n\nThe note we received from your administrator was: "${message}"\n\nYour contact information has been kept confidential. This message was written without knowledge of who sent the feedback.\n\nHere is your administrator's email if you want to reply: ${adminEmail}. I hope to here more of your thoughts in the future!\n\nYour friend at Suggestion Box.`;
-  return { subjectLine, bodyText };
-}
-
-function clarifySolution({ solution, message, adminEmail }) {
-  const subjectLine = 'Suggestion Box: Clarification needed';
-  const bodyText =
-      `Hi! This is your friend at Suggestion Box.\n\nThank you for proposing a solution with Suggestion Box! Your administrator has requested for a clarification on your solution: "${solution.text}".\n\nThe note we received from your administrator was: "${message}"\n\nYour contact information has been kept confidential. This message was written without knowledge of who sent the solution.\n\nHere is your administrator's email if you want to reply: ${adminEmail}. I hope to here more of your thoughts in the future!\n\nYour friend at Suggestion Box.`;
   return { subjectLine, bodyText };
 }
 
