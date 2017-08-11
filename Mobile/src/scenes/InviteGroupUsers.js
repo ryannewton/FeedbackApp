a12981dbe677c843b6a74357485662bcf50e2f3f
Alice Zheng <alicezheng@Alices-MacBook-Pro-3.local>
2017-08-11 13:12:23 -0700

    style for sent and image size

--- Mobile/src/scenes/InviteGroupUsers.js
+++ Mobile/src/scenes/InviteGroupUsers.js
@@ -32,7 +32,7 @@ class InviteGroupUsers extends Component {
   renderSentNotification() {
     if (this.state.showSentNotfication) {
       return (
-        <Text>
+        <Text style={[styles.errorTextStyle, {color:'#48D2A0'}]}>
           Sent!
         </Text>
       );
@@ -42,7 +42,7 @@ class InviteGroupUsers extends Component {
   renderErrorMessage() {
     if (this.state.showErrorMessage) {
       return (
-        <Text>
+        <Text style={styles.errorTextStyle}>
           Please enter a valid email address.
         </Text>
       );
